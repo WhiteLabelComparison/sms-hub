c96877ccff3eb87dc8fd6734f0dcd2b6aeae1d10
James Read <james.read.18@gmail.com>
2017-07-12 12:52:40 +0100

    wip: finished storing files in s3 and database

--- src/app/controllers/receive.controller.ts
+++ src/app/controllers/receive.controller.ts
@@ -48,40 +48,56 @@ export class ReceiveController {
     static saveEmail(to: string, from: string, subject: string, message: string, timestamp: string, attachments: MailAttachment[]) {
         Log.debug("Saving message to " + to + " from " + from);
 
-        if (attachments) {
-            aws.config.update({ accessKeyId: Config.aws.key, secretAccessKey: Config.aws.secret, signatureVersion: 'v4' });
-            let s3Client = new aws.S3();
+        let db = new Database().db;
+        return db.one("INSERT INTO conversations (type, outbound_number, inbound_number, subject, content, message_count, created_at) VALUES('email',$[from], $[to], $[subject], $[message], 0, NOW()) RETURNING id", {
+            to: to,
+            from: from,
+            subject: subject,
+            message: message,
+            timestamp: timestamp
+        }).then(result => {
+            if (attachments) {
+                Log.debug("Email has attachments");
 
-            // get the date and time as string YYYMMDD/HHMMSS
-            let date = new Date;
-            let dateStr = [
-                [date.getFullYear().toString(), ("0" + (date.getMonth() + 1)).slice(-2), ("0" + date.getDate()).slice(-2)].join(''),
-                [("0" + date.getHours()).slice(-2), ("0" + date.getMinutes()).slice(-2), ("0" + date.getSeconds()).slice(-2)].join('')
-            ].join('/');
+                // init s3 client
+                aws.config.update({ accessKeyId: Config.aws.key, secretAccessKey: Config.aws.secret, signatureVersion: 'v4' });
+                let s3Client = new aws.S3();
 
-            for (let file of attachments) {
-                let remotePath = "emails/" + from + "/" + dateStr + "-" + file.filename;
-                s3Client.putObject({
-                    Bucket: Config.bucket,
-                    Key: remotePath,
-                    Body: file.content,
-                    ACL: 'public-read'
-                }, (err, data) => {
-                    console.log(data);
-                });
+                // get the date and time as string YYYMMDD/HHMMSS
+                let date = new Date;
+                let dateStr = [
+                    [date.getFullYear().toString(), ("0" + (date.getMonth() + 1)).slice(-2), ("0" + date.getDate()).slice(-2)].join(''),
+                    [("0" + date.getHours()).slice(-2), ("0" + date.getMinutes()).slice(-2), ("0" + date.getSeconds()).slice(-2)].join('')
+                ].join('/');
+
+                for (let file of attachments) {
+                    let remotePath = "emails/" + from + "/" + dateStr + "-" + file.filename;
+                    s3Client.putObject({
+                        Bucket: Config.bucket,
+                        Key: remotePath,
+                        Body: file.content,
+                        ACL: 'public-read'
+                    }, (err, data) => {
+                        if (err) {
+                            Log.error("filed to upload " + file.filename);
+                            return;
+                        }
+
+                        db.none(
+                            "INSERT INTO conversation_attachments (conversation_id, filename, url) VALUES (${conversation_id}, ${filename}, ${url})",
+                            {
+                                "conversation_id": result.id,
+                                "filename": file.filename,
+                                "url": "https://s3.eu-west-2.amazonaws.com/".concat(Config.bucket, '/', remotePath)
+                            }
+                        ).then(() => {})
+                        .catch((err) => {
+                            Log.error("Failed to insert attachment");
+                        });
+                    });
+                }
             }
-        }
-
-        return new Promise((res, err) => { });
-
-        // let db = new Database().db;
-        // return db.one("INSERT INTO conversations (type, outbound_number, inbound_number, subject, content, message_count, created_at) VALUES('email',$[from], $[to], $[subject], $[message], 0, NOW()) RETURNING id", {
-        //     to: to,
-        //     from: from,
-        //     subject: subject,
-        //     message: message,
-        //     timestamp: timestamp
-        // });
+        });
     }
 
     static saveMessage(to: string, from: string, message: string, timestamp: string) {
